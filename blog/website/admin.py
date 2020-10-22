dde0b1357a3744953889a948e2cc68c3c6ac4dd7
Luciano <lopezz.lucio85@gmail.com>
2020-10-22 17:37:19 -0300

    personalizando o admin

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin 
 from .models import Post 
 
-admin.site.register(Post) 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','sub_title','full_name')
+    search_fields = ('title','sub_title')
+
+admin.site.register(Post, PostAdmin) 
--- blog/website/models.py
+++ blog/website/models.py
@@ -4,4 +4,12 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def full_name(self): 
+        return self.title + self.sub_title 
+
+    full_name.admin_order_field = 'title'
     

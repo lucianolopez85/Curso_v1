3797ebb417f253cfd71d5057daffe9ae0e010814
Luciano <lopezz.lucio85@gmail.com>
2020-10-23 16:07:48 -0300

    criado categoria no display (TextChoises)

--- blog/website/admin.py
+++ blog/website/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Post 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','sub_title','full_name')
+    list_display = ('title','sub_title','categories')   # 'full_name'
     search_fields = ('title','sub_title')
 
 admin.site.register(Post, PostAdmin) 
--- blog/website/models.py
+++ blog/website/models.py
@@ -1,15 +1,28 @@
 from django.db import models
 
+class Category(models.TextChoices):
+    TECH = 'TC','Tecnologia'
+    CR = 'CR','Curiosidade'
+    GR = 'GR', 'Geral'
+
+    
 class Post(models.Model):
     title = models.CharField(max_length=100)
     sub_title = models.CharField(max_length=200)
     content = models.TextField()
+    categories = models.CharField(
+        max_length=2,
+        choices=Category.choices,
+        default=Category.GR,
+    )  
+
 
     def __str__(self):
         return self.title
 
-    def full_name(self): 
-        return self.title + self.sub_title 
 
-    full_name.admin_order_field = 'title'
+    #def full_name(self):    => função que junta os titulos
+     #   return self.title + self.sub_title 
+
+    categories.admin_order_field = 'categories'
     
